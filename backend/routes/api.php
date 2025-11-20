17a6213b72058017e09ba979159c441d6527ce5f
Rayyane <akibmr9@gmail.com>
2025-11-21 00:40:25 +0600

    backend authentication logic applied

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class);
+    Route::post('/register', RegisterController::class);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
